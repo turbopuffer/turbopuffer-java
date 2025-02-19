845f26eb563ffc1b899c107af031f12418b017b7
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-02-19 21:36:25 +0000

    feat: fix: Javadoc publishing task

--- turbopuffer-java/build.gradle.kts
+++ turbopuffer-java/build.gradle.kts
@@ -6,3 +6,26 @@ plugins {
 dependencies {
     api(project(":turbopuffer-java-client-okhttp"))
 }
+
+// Redefine `dokkaJavadoc` to:
+// - Depend on the root project's task for merging the docs of all the projects
+// - Forward that task's output to this task's output
+//
+// Taken from OpenAI's Java client: https://github.com/openai/openai-java/blob/28e23c9ed1110ad6216e3d83c8be5cfae0de09ae/openai-java/build.gradle.kts
+tasks.named("dokkaJavadoc").configure {
+    actions.clear()
+
+    val dokkaJavadocCollector = rootProject.tasks["dokkaJavadocCollector"]
+    dependsOn(dokkaJavadocCollector)
+
+    val outputDirectory = project.layout.buildDirectory.dir("dokka/javadoc")
+    doLast {
+        copy {
+            from(dokkaJavadocCollector.outputs.files)
+            into(outputDirectory)
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+
+    outputs.dir(outputDirectory)
+}
