e207ac0c97b145963822ecf631555077bb4c4bf5
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-02-19 16:51:34 -0500

    fix: avoid races in dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,12 @@ allprojects {
 
     apply(plugin = "org.jetbrains.dokka")
 }
+
+// Avoid race conditions between `dokkaJavadocCollector` and `dokkaJavadocJar` tasks.
+//
+// Taken from OpenAI's Java client: https://github.com/openai/openai-java/blob/28e23c9ed1110ad6216e3d83c8be5cfae0de09ae/build.gradle.kts
+tasks.named("dokkaJavadocCollector").configure {
+    subprojects.flatMap { it.tasks }
+        .filter { it.project.name != "turbopuffer-java" && it.name == "dokkaJavadocJar" }
+        .forEach { mustRunAfter(it) }
+}
