50fa88c868c226f840ab568e22e3a4bb2d75ed1d
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-02-19 14:48:31 -0500

    fix: generate Javadocs on publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
+plugins {
+    id("org.jetbrains.dokka") version "2.0.0"
+}
+
 allprojects {
     group = "com.turbopuffer"
     version = "0.1.0-beta.0" // x-release-please-version
+
+    repositories {
+        mavenCentral()
+    }
+
+    apply(plugin = "org.jetbrains.dokka")
 }
