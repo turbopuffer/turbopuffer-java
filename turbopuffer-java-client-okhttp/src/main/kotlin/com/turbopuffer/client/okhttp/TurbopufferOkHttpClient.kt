33bbd4d3d6c8244922837423ac8c5e6458e47ac1
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-06-23 16:29:40 -0400

    feat: allow configuring maxRequests
    For workloads that need high concurrency (e.g., write workloads that for
    some reason can't do batching).

--- turbopuffer-java-client-okhttp/src/main/kotlin/com/turbopuffer/client/okhttp/TurbopufferOkHttpClient.kt
+++ turbopuffer-java-client-okhttp/src/main/kotlin/com/turbopuffer/client/okhttp/TurbopufferOkHttpClient.kt
@@ -29,6 +29,7 @@ class TurbopufferOkHttpClient private constructor() {
         private var clientOptions: ClientOptions.Builder = ClientOptions.builder()
         private var timeout: Timeout = Timeout.default()
         private var proxy: Proxy? = null
+        private var maxRequests: Int = 64
 
         fun baseUrl(baseUrl: String) = apply { clientOptions.baseUrl(baseUrl) }
 
@@ -143,6 +144,8 @@ class TurbopufferOkHttpClient private constructor() {
 
         fun maxRetries(maxRetries: Int) = apply { clientOptions.maxRetries(maxRetries) }
 
+        fun maxRequests(maxRequests: Int) = apply { this.maxRequests = maxRequests }
+
         fun proxy(proxy: Proxy) = apply { this.proxy = proxy }
 
         fun responseValidation(responseValidation: Boolean) = apply {
@@ -166,6 +169,7 @@ class TurbopufferOkHttpClient private constructor() {
                             .baseUrl(clientOptions.baseUrl())
                             .timeout(timeout)
                             .proxy(proxy)
+                            .maxRequests(maxRequests)
                             .build()
                     )
                     .build()
