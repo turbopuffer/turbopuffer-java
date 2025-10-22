e4c180204bcee950755899d888f1f8c7e2cab498
Xavier Denis <xldenis@gmail.com>
2025-10-22 12:55:40 +0200

    chore: adjust type of filter for patch_by_filter (#190)

--- turbopuffer-java-core/src/test/kotlin/com/turbopuffer/services/blocking/NamespaceServiceTest.kt
+++ turbopuffer-java-core/src/test/kotlin/com/turbopuffer/services/blocking/NamespaceServiceTest.kt
@@ -8,6 +8,7 @@ import com.turbopuffer.core.JsonValue
 import com.turbopuffer.models.namespaces.AttributeSchemaConfig
 import com.turbopuffer.models.namespaces.Columns
 import com.turbopuffer.models.namespaces.DistanceMetric
+import com.turbopuffer.models.namespaces.Filter
 import com.turbopuffer.models.namespaces.NamespaceDeleteAllParams
 import com.turbopuffer.models.namespaces.NamespaceExplainQueryParams
 import com.turbopuffer.models.namespaces.NamespaceHintCacheWarmParams
@@ -265,7 +266,7 @@ internal class NamespaceServiceTest {
                     )
                     .patchByFilter(
                         NamespaceWriteParams.PatchByFilter.builder()
-                            .filters(JsonValue.from(mapOf<String, Any>()))
+                            .filters(Filter.eq("attr", "value"))
                             .patch(
                                 NamespaceWriteParams.PatchByFilter.Patch.builder()
                                     .putAdditionalProperty("foo", JsonValue.from("bar"))
